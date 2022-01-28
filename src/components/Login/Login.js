42c685908d51d4e016f910db755a465d53dfa945
Petar-I-Petkov <cpt.petkov@gmail.com>
2022-01-28 13:22:21 +0200

    save
    #16

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,16 @@
 
-
+import { ReactComponent as PinkCarpet } from './assets/PinkCarpet.svg'
 import LoginForm from 'components/Login/LoginForm/LoginForm';
+
 
 import './Login.scss';
 const Login = () => {
     return (
         <section className="login">
+            <aside className="left-graphics">
+                <PinkCarpet className="pink-carpet" />
+                <a className="credit" href="https://www.freepik.com/vectors/food">Food vector created by stories - www.freepik.com</a>
+            </aside>
             <LoginForm />
         </section>
     )
--- src/components/Login/LoginForm/LoginForm.js
+++ src/components/Login/LoginForm/LoginForm.js
@@ -1,9 +1,59 @@
-import './LoginForm.scss';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
+import { loginFormSchema } from './validations/loginFormSchema';
+import InputWithCustomPlaceholder from 'components/shared/InputWithCustomPlaceholder/InputWithCustomPlaceholder';
+import { ReactComponent as ToiletPaper } from '../assets/ToiletPaper.svg'
+
+
+import './LoginForm.scss';
 const LoginForm = () => {
+
+    const [inputValues, setInputValues] = useState({});
+    const [errors, setErrors] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const publishInputValue = (inputName, value) => {
+        setInputValues((inputValues) => ({
+            ...inputValues,
+            [inputName]: value,
+        }));
+    };
     return (
-        <section className="loginForm">
-            Login
+        <section className="login-form">
+            <div className="top-icon">
+                <ToiletPaper className="top-icon-image" />
+            </div>
+            <form className="form">
+                <div className='input-container'>
+                    <InputWithCustomPlaceholder
+                        name="email"
+                        placeholder={'email'}
+                        publishInputValue={publishInputValue}
+                        globalValidationError={errors.email}
+                        hasHideIcon={false}
+                        isSubmitted={isSubmitted}
+                        validations={loginFormSchema}
+                    />
+                </div>
+                <div className='input-container'>
+                    <InputWithCustomPlaceholder
+                        name="password"
+                        placeholder={'password'}
+                        publishInputValue={publishInputValue}
+                        globalValidationError={errors.password}
+                        hasHideIcon={true}
+                        isSubmitted={isSubmitted}
+                        validations={loginFormSchema}
+                    />
+                </div>
+                <section className="links-container">
+                    <p className="login-submit">Login</p>
+                    <p className="or">or</p>
+                    <p className="signup">Signup</p>
+                </section>
+            </form>
         </section>
     )
 }
