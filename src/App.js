a59bd5615c281f03671be4c763ecbbd064328248
Petar-I-Petkov <cpt.petkov@gmail.com>
2022-01-30 07:56:22 +0200

    clean up unsude items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import { Routes, Route } from "react-router-dom"
-import { useContext } from 'react';
-import { useAuth } from 'contexts/AuthContext';
 
-import * as authService from 'services/authService';
-import * as userService from 'services/userService';
-
-import Dev from 'components/Dev';
 import RegisterOrganizationOwner from 'components/RegisterOrganizationOwner/RegisterOrganizationOwner';
 import Login from 'components/Login/Login';
 
@@ -17,9 +11,7 @@ function App() {
       <Routes>
         <Route path="/register-organization-owner" element={<RegisterOrganizationOwner />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dev" element={<Dev />} />
         <Route path="/.well-known/first-party-set" element={JSON.stringify("test")} />
-
       </Routes>
     </div>
   );
