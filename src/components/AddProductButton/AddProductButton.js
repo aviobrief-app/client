6ed318d0e7675ef6d549d2ccd22cb3c353a604f8
Petar-I-Petkov <cpt.petkov@gmail.com>
2022-02-04 04:30:39 +0200

    constructed the Add Button
    #20

--- src/components/AddProductButton/AddProductButton.js
+++ src/components/AddProductButton/AddProductButton.js
@@ -1,22 +1,14 @@
 
-
-import ButtonFilled from 'components/shared/ButtonFilled/ButtonFilled';
+import { ReactComponent as ThinPlusSign } from 'assets/svg/ThinPlusSign.svg';
 
 
 import './AddProductButton.scss';
 const AddProductButton = () => {
     return (
-        <div className="add-product-button">
-            <ButtonFilled
-                fillColor="#9A2FAE"
-                text="Add"
-                width="120px"
-                height="40px"
-                fontSize="inherit"
-                fontWeight="500"
-
-            />
-        </div>
+        <section className="add-product-button">
+            <ThinPlusSign />
+            <p className="button-text">Add</p>
+        </section>
     )
 }
 
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,20 +1,17 @@
 
 import { ReactComponent as ThinPlusSign } from 'assets/svg/ThinPlusSign.svg';
 import AddProductButton from 'components/AddProductButton/AddProductButton';
+
+
 import './Dashboard.scss';
 const Dashboard = () => {
     return (
         <section className="dashboard">
-            <div className="add-button-container">
-                <div className="plus-sign">
-                    <ThinPlusSign />
-                </div>
-                <div className="add-button">
-                    <AddProductButton />
-                </div>
-            </div>
-        </section>
 
+            <section className="add-button">
+                <AddProductButton />
+            </section>
+        </section>
     )
 }
 
