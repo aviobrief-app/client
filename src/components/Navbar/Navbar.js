3f1086138fcb346780cb724fab77d95f6ab36cb3
Petar-I-Petkov <cpt.petkov@gmail.com>
2022-02-04 05:18:26 +0200

    navbar finished so far
    #20

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,24 @@
 
 
-
+import { ReactComponent as MobileMenu } from 'assets/svg/MobileMenu.svg';
+import ShoppingBagWithItemsCount from 'components/ShoppingBagWithItemsCount/ShoppingBagWithItemsCount';
 
 import './Navbar.scss';
 const Navbar = () => {
     return (
-        <section className="navbar">
-            NAVBAR
-        </section>
+        <nav className="navbar">
+            <section className="left-section">
+                <MobileMenu className="mobile-menu" />
+                <div className="user-info">
+                    <p className="username">Dian</p>
+                    <p className="organization-name">Dian and Moni's home</p>
+                </div>
+            </section>
+            <section className="right-section">
+                <ShoppingBagWithItemsCount />
+            </section>
+
+        </nav>
     )
 }
 
---
+++ src/components/ShoppingBagWithItemsCount/ShoppingBagWithItemsCount.js
@@ -0,0 +1,14 @@
+
+
+import { ReactComponent as ShoppingBagWhite } from 'assets/svg/ShoppingBagWhite.svg'
+import './ShoppingBagWithItemsCount.scss';
+const ShoppingBagWithItemsCount = () => {
+    return (
+        <div>
+            <ShoppingBagWhite className="shopping-bag-white" />
+        </div>
+    )
+}
+
+
+export default ShoppingBagWithItemsCount;
