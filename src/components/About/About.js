436063d45b7537417d2143cb49f9590abd6661c0
Petar-I-Petkov <cpt.petkov@gmail.com>
2022-02-11 12:32:14 +0200

    right part is ready

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,6 @@
+import { Link } from 'react-router-dom';
 
-
+import { ReactComponent as AboutPageBackground } from './assets/AboutPageBackground.svg';
 
 import './About.scss';
 
@@ -7,6 +8,17 @@ import './About.scss';
 const About = () => {
     return (
         <section className="about">
+            <section className="left">
+
+            </section>
+            <section className="right">
+                <nav className="about-menu">
+                    <Link to="">How it works</Link>
+                    <Link to="">About us</Link>
+                    <Link to="">Contacts</Link>
+                </nav>
+                <AboutPageBackground className="background" />
+            </section>
 
         </section>
     )
