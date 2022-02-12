07d3c7f2a088ff0f4bb18f1a1b214f3adb08e88c
Petar-I-Petkov <cpt.petkov@gmail.com>
2022-02-12 05:34:42 +0200

    mui button color changed
    #29

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
+import { ThemeProvider } from '@mui/styles';
+import { createTheme } from '@mui/material/styles';
+
 
 import About from 'components/About/About';
 import Login from 'components/Login/Login';
@@ -11,20 +14,49 @@ import withNavbar from 'components/hoc/withNavbar';
 import './App.css';
 function App() {
 
+  const theme = createTheme({
+    typography: {
+      // palette: {
+      //   primary: {
+      //     light: "#9a2fae",
+      //     main: "#9a2fae",
+      //     dark: "#9a2fae",
+
+      //   },
+      //   secondary: {
+      //     main: "#9a2fae"
+      //   },
+      //   info: {
+      //     light: "#9a2fae",
+      //     main: "#9a2fae",
+      //     dark: "#9a2fae",
+      //   }
+      // },
+      fontFamily: [
+        'Roboto',
+        'sans-serif'
+      ].join(','),
+    }
+  });
+
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<About />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register-organization-owner" element={<RegisterOrganizationOwner />} />
+    <ThemeProvider theme={theme}>
 
-        <Route path="/dashboard" element={withNavbar(Dashboard)} />
-        <Route path="/purchases" element={withNavbar(Purchases)} />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register-organization-owner" element={<RegisterOrganizationOwner />} />
+
+          <Route path="/dashboard" element={withNavbar(Dashboard)} />
+          <Route path="/purchases" element={withNavbar(Purchases)} />
 
 
-        <Route path="/.well-known/first-party-set" element={JSON.stringify("test")} />
-      </Routes>
-    </div>
+          <Route path="/.well-known/first-party-set" element={JSON.stringify("test")} />
+        </Routes>
+      </div>
+    </ThemeProvider>
+
   );
 }
 
--- src/components/Purchases/Purchases.js
+++ src/components/Purchases/Purchases.js
@@ -1,10 +1,23 @@
 
+import { Button } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 
 import './Purchases.scss';
 const Purchases = () => {
+
+
+
+
     return (
-        <section className="to-buy">
-            to buy
+        <section className="purchases">
+            <Button
+                className="add-button"
+                variant="contained"
+                startIcon={<AddIcon className="add-icon" />}
+            >
+                Add
+            </Button>
+
         </section>
     )
 
