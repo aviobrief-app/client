e2afba08010903ac924f02bdea07285788d5cb4e
Petar-I-Petkov <cpt.petkov@gmail.com>
2022-02-12 06:32:03 +0200

    filter section is ready
    #29

--- src/components/Purchases/Purchases.js
+++ src/components/Purchases/Purchases.js
@@ -1,6 +1,9 @@
 
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+import FilterListIcon from '@mui/icons-material/FilterList';
+
+import SearchBarWithIcon from 'components/shared/SearchBarWithIcon/SearchBarWithIcon';
 
 import './Purchases.scss';
 const Purchases = () => {
@@ -10,13 +13,28 @@ const Purchases = () => {
 
     return (
         <section className="purchases">
-            <Button
-                className="add-button"
-                variant="contained"
-                startIcon={<AddIcon className="add-icon" />}
-            >
-                Add
-            </Button>
+            <section className="filter-section">
+                <span className="buttons-wrapper">
+                    <Button
+                        className="add-button"
+                        variant="contained"
+                        startIcon={<AddIcon className="add-icon" />}
+                    >
+                        Add
+                    </Button>
+                    <Button
+                        className="filter-button"
+                        variant="contained"
+                        startIcon={<FilterListIcon className="add-icon" />}
+                    >
+                        Filter
+                    </Button>
+                </span>
+                <span className="search-field">
+                    <SearchBarWithIcon placeholder="search to buy..." />
+
+                </span>
+            </section>
 
         </section>
     )
---
+++ src/components/shared/SearchBarWithIcon/SearchBarWithIcon.js
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+import './SearchBarWithIcon.scss';
+
+
+const SearchBarWithIcon = ({
+    placeholder
+}) => {
+
+    return (
+        <Paper
+            component="form"
+            className="search-bar-with-icon"
+        >
+
+            <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                className="input-base"
+                placeholder={placeholder}
+                inputProps={{ 'aria-label': placeholder }}
+            />
+            <IconButton type="submit" aria-label="search" className="input-icon">
+                <SearchIcon />
+            </IconButton>
+
+        </Paper>
+    );
+
+}
+
+
+export default SearchBarWithIcon;
