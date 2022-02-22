07b9d721e7095c3c8097a0a19bdd6fa6c7ac2f1a
Petar-I-Petkov <cpt.petkov@gmail.com>
2022-02-22 06:07:47 +0200

    updated navbar
    #35

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,19 @@
-
+import { useAuth } from 'contexts/AuthContext';
 
 import { ReactComponent as MobileMenu } from 'assets/svg/MobileMenu.svg';
 import ShoppingBagWithItemsCount from 'components/shared/ShoppingBagWithItemsCount/ShoppingBagWithItemsCount';
 
 import './Navbar.scss';
 const Navbar = () => {
+    const { currentUserClaims } = useAuth();
+
     return (
         <nav className="navbar">
             <section className="left-section">
                 <MobileMenu className="mobile-menu" />
                 <div className="user-info">
-                    <p className="username">Dian</p>
-                    <p className="organization-name">Dian and Moni's home</p>
+                    <p className="username">{currentUserClaims.fullName.split(' ')[0]}</p>
+                    <p className="organization-name">{currentUserClaims.organizationName}</p>
                 </div>
             </section>
             <section className="right-section">
